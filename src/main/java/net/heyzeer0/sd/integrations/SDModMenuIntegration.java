8fa4a220559e4e8850c55aac094af7a6b073472c
someone5678 <nemui3353@gmail.com>
2021-12-30 22:30:54 +0900

    update to support 1.18.x

--- src/main/java/net/heyzeer0/sd/integrations/SDModMenuIntegration.java
+++ src/main/java/net/heyzeer0/sd/integrations/SDModMenuIntegration.java
@@ -1,7 +1,7 @@
 package net.heyzeer0.sd.integrations;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.heyzeer0.sd.configs.GeneralConfig;
 
